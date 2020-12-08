4adee8de56868f33730c4422b7ebcd0954eda851
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-08 23:12:02 +0100

    readAllTasks()/TaskController created

--- src/main/java/com/example/taskmanager/controller/TaskController.java
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -1,17 +1,26 @@
 package com.example.taskmanager.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.taskmanager.repository.TaskRepository;
 
 @RepositoryRestController
 class TaskController {
 
-	
+	private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 	private final TaskRepository repository;
 
 	public TaskController(TaskRepository repository) {
 		this.repository = repository;
+	}
+
+	@GetMapping("/tasks")
+	ResponseEntity<?> readAllTasks() {
+		logger.warn("Exposing all tasks!");
+		return ResponseEntity.ok(repository.findAll());
 	}
 }
