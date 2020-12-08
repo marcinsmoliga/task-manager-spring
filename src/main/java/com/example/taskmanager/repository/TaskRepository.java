04c7ed63a3fe6079d74cc798818ee9265aa1b449
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-08 22:00:06 +0100

    @RestResource(exported = false) tested

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -2,9 +2,18 @@ package com.example.taskmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.taskmanager.model.Task;
 
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task, Integer> {
+
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Integer integer);
+
+	@Override
+    @RestResource(exported = false)
+	void delete(Task task);
 }
