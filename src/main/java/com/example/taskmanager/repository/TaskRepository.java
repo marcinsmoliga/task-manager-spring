2e24f807816a25bec57d8f9bc46cc6d6727b61ce
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-08 22:11:15 +0100

    findByDone(boolean done)/TaskRepository created

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,6 +1,9 @@
 package com.example.taskmanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -16,4 +19,7 @@ interface TaskRepository extends JpaRepository<Task, Integer> {
 	@Override
     @RestResource(exported = false)
 	void delete(Task task);
+
+	@RestResource(path = "done", rel = "done")
+	List<Task> findByDone(@Param("state") boolean done);
 }
