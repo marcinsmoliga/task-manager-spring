95139e3d4c9d5cb8a50eac9c5626799d64bbc539
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-09 16:19:23 +0100

    readAllTasks(Pageable page)/Taskcontroller created

--- src/main/java/com/example/taskmanager/controller/TaskController.java
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.taskmanager.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,15 @@ class TaskController {
 		this.repository = repository;
 	}
 
-	@GetMapping("/tasks")
+	@GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
 	ResponseEntity<?> readAllTasks() {
 		logger.warn("Exposing all tasks!");
 		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/tasks")
+	ResponseEntity<?> readAllTasks(Pageable page) {
+		logger.info("Custom pageable");
+		return ResponseEntity.ok(repository.findAll(page));
 	}
 }
