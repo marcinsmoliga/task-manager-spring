1b249409cbdeabcabcdb3a20101dd2fe8f255ed1
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-13 11:24:51 +0100

    TaskGroupRepositery created | several methods added

---
+++ src/main/java/com/example/taskmanager/repository/TaskGroupRepository.java
@@ -0,0 +1,12 @@
+package com.example.taskmanager.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.taskmanager.model.TaskGroup;
+
+public interface TaskGroupRepository {
+	List<TaskGroup> findAll();
+	Optional<TaskGroup> findById(Integer id);
+	TaskGroup save(TaskGroup entity);
+}
