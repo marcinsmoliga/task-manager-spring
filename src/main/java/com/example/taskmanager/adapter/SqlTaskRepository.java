76cfc4fddf9efa3e3096c1e6f2718634a84b2bc7
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-13 11:34:41 +0100

    interface SqlTaskGroupRepository/adapter created

--- src/main/java/com/example/taskmanager/adapter/SqlTaskRepository.java
+++ src/main/java/com/example/taskmanager/adapter/SqlTaskRepository.java
@@ -1,4 +1,4 @@
-package com.example.taskmanager.repository;
+package com.example.taskmanager.adapter;
 
 import java.util.List;
 
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task,Integer> {
