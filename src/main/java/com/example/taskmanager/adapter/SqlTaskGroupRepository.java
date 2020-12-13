ef8e78708b7f1666dcbbd07d95ad3d4d4869919a
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-13 12:13:21 +0100

    TemService/logic created

--- src/main/java/com/example/taskmanager/adapter/SqlTaskGroupRepository.java
+++ src/main/java/com/example/taskmanager/adapter/SqlTaskGroupRepository.java
@@ -1,6 +1,9 @@
 package com.example.taskmanager.adapter;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.taskmanager.model.TaskGroup;
@@ -8,4 +11,8 @@ import com.example.taskmanager.repository.TaskGroupRepository;
 
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+
+	@Override
+	@Query("from TaskGroup g join fetch g.tasks")
+	List<TaskGroup> findAll();
 }
