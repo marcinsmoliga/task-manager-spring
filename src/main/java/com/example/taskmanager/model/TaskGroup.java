33541a663165216270a72798b0b1cfa5b3003630
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-15 14:40:49 +0100

    class GroupWriteModel/projection | fields added

--- src/main/java/com/example/taskmanager/model/TaskGroup.java
+++ src/main/java/com/example/taskmanager/model/TaskGroup.java
@@ -53,7 +53,7 @@ public class TaskGroup {
 		return description;
 	}
 
-	void setDescription(String description) {
+	public void setDescription(String description) {
 		this.description = description;
 	}
 
@@ -69,8 +69,16 @@ public class TaskGroup {
 		return tasks;
 	}
 
-	void setTasks(Set<Task> tasks) {
+	public void setTasks(Set<Task> tasks) {
 		this.tasks = tasks;
 	}
 
+
+	Project getProject() {
+		return project;
+	}
+
+	void setProject(Project project) {
+		this.project = project;
+	}
 }
---
+++ src/main/java/com/example/taskmanager/model/projection/GroupWriteModel.java
@@ -0,0 +1,39 @@
+package com.example.taskmanager.model.projection;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.example.taskmanager.model.TaskGroup;
+
+public class GroupWriteModel {
+
+	private String description;
+	private Set<GroupTaskWriteModel> tasks;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<GroupTaskWriteModel> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(Set<GroupTaskWriteModel> tasks) {
+		this.tasks = tasks;
+	}
+
+	public TaskGroup toGroup() {
+		var result = new TaskGroup();
+		result.setDescription(description);
+		result.setTasks(
+				tasks.stream()
+				.map(GroupTaskWriteModel::toTask)
+				.collect(Collectors.toSet())
+		);
+		return result;
+	}
+}
