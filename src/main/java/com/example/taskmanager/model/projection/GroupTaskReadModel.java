481445341ac314e7fe7eae8ff43f2f02478a1e4c
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-15 14:17:13 +0100

    class GroupTaskReadModel, GroupTaskWriteModel/projection/model created | fields added

---
+++ src/main/java/com/example/taskmanager/model/projection/GroupTaskReadModel.java
@@ -0,0 +1,30 @@
+package com.example.taskmanager.model.projection;
+
+import com.example.taskmanager.model.Task;
+
+public class GroupTaskReadModel {
+
+	private String description;
+	private boolean done;
+
+	GroupTaskReadModel(Task source) {
+		description = source.getDescription();
+		done = source.isDone();
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+}
---
+++ src/main/java/com/example/taskmanager/model/projection/GroupTaskWriteModel.java
@@ -0,0 +1,31 @@
+package com.example.taskmanager.model.projection;
+
+import java.time.LocalDateTime;
+
+import com.example.taskmanager.model.Task;
+
+public class GroupTaskWriteModel {
+
+	private String description;
+	private LocalDateTime deadline;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(LocalDateTime deadline) {
+		this.deadline = deadline;
+	}
+
+	public Task toTask() {
+		return new Task(description, deadline);
+	}
+}
