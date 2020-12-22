784f88d8546d6c3f537e4da39f6e856dc1a0f53f
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-22 14:26:57 +0100

    createGroup()/ProjectService refactored

--- src/main/java/com/example/taskmanager/logic/ProjectService.java
+++ src/main/java/com/example/taskmanager/logic/ProjectService.java
@@ -11,17 +11,24 @@ import com.example.taskmanager.model.Project;
 import com.example.taskmanager.model.Task;
 import com.example.taskmanager.model.TaskGroup;
 import com.example.taskmanager.model.projection.GroupReadModel;
+import com.example.taskmanager.model.projection.GroupTaskWriteModel;
+import com.example.taskmanager.model.projection.GroupWriteModel;
 import com.example.taskmanager.repository.ProjectRepository;
 import com.example.taskmanager.repository.TaskGroupRepository;
 
 public class ProjectService {
 	private ProjectRepository repository;
 	private TaskGroupRepository taskGroupRepository;
+	private TaskGroupService service;
 	private TaskConfigurationProperties config;
 
-	public ProjectService(final ProjectRepository repository, final TaskGroupRepository taskGroupRepository, final TaskConfigurationProperties config) {
+	public ProjectService(final ProjectRepository repository,
+	                      final TaskGroupRepository taskGroupRepository,
+	                      final TaskGroupService service,
+	                      final TaskConfigurationProperties config) {
 		this.repository = repository;
 		this.taskGroupRepository = taskGroupRepository;
+		this. service = service;
 		this.config = config;
 	}
 
@@ -37,20 +44,22 @@ public class ProjectService {
 		if (!config.getTemplate().isAllowMultipleTasks() && taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
 			throw new IllegalStateException("Only one undone group from project is allowed");
 		}
-		TaskGroup result = repository.findById(projectId)
+		GroupReadModel result = repository.findById(projectId)
 				.map(project -> {
-					var targetGroup = new TaskGroup();
+					var targetGroup = new GroupWriteModel();
 					targetGroup.setDescription(project.getDescription());
 					targetGroup.setTasks(
 							project.getSteps().stream()
-									.map(projectStep -> new Task(
-											projectStep.getDescription(),
-											deadline.plusDays(projectStep.getDaysToDeadline()))
+									.map(projectStep -> {
+										var task = new GroupTaskWriteModel();
+										task.setDescription(projectStep.getDescription());
+										task.setDeadline(deadline.plusDays(projectStep.getDaysToDeadline()));
+										return  task;
+									}
 									).collect(Collectors.toSet())
 					);
-					targetGroup.setProject(project);
-					return taskGroupRepository.save(targetGroup);
+					return service.createGroup(targetGroup);
 				}).orElseThrow(() -> new IllegalArgumentException("Project with given id not found"));
-		return new GroupReadModel(result);
+		return result;
 	}
 }
