b2c16b77fa76dcd548ab2dc99e07dbabdae3081b
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-22 13:40:21 +0100

    test void httpGet_returnsGivenTask created

--- src/test/java/com/example/taskmanager/controller/TaskControllerIntegrationTest.java
+++ src/test/java/com/example/taskmanager/controller/TaskControllerIntegrationTest.java
@@ -1,4 +1,42 @@
 package com.example.taskmanager.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
 class TaskControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private TaskRepository repo;
+
+	@Test
+	void httpGet_returnsGivenTask() throws Exception {
+		// given
+		int id = repo.save(new Task("foo", LocalDateTime.now())).getId();
+
+		// when + then
+		mockMvc.perform(get("/tasks/" + id))
+				.andExpect(status().is2xxSuccessful());
+	}
+
 }
