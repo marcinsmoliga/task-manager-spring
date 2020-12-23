fe2b34a3ca11acd3d40e9edb0d38efadd3442cab
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-23 16:50:40 +0100

    doFilter()/LoggerFilter created

--- src/main/java/com/example/taskmanager/controller/LoggerFilter.java
+++ src/main/java/com/example/taskmanager/controller/LoggerFilter.java
@@ -1,4 +1,30 @@
 package com.example.taskmanager.controller;
 
-class LoggerFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+class LoggerFilter implements Filter {
+	private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+	                     FilterChain filterChain) throws IOException, ServletException {
+
+		if (servletRequest instanceof HttpServletRequest) {
+			var httpRequest = (HttpServletRequest) servletRequest;
+			logger.info("[doFilter]" + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+		}
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
 }
