9dced695209fa086accfcafcca984d77279d547d
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-24 14:49:01 +0100

    field Set<HandlerInterceptor> interceptors/MvcConfiguration added

--- src/main/java/com/example/taskmanager/controller/MvcConfiguration.java
+++ src/main/java/com/example/taskmanager/controller/MvcConfiguration.java
@@ -1,14 +1,24 @@
 package com.example.taskmanager.controller;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 class MvcConfiguration implements WebMvcConfigurer {
 
+	private final Set<HandlerInterceptor> interceptors;
+
+	public MvcConfiguration(Set<HandlerInterceptor> interceptors) {
+		this.interceptors = interceptors;
+	}
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoggerInterceptor());
+		interceptors.forEach(registry::addInterceptor);
 	}
 }
