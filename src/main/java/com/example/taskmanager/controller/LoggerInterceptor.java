2b57fd0a2f14fb0d18fbaa114e2d61881a1c9f9d
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-24 14:22:55 +0100

    addInterceptors()/MvcConfiguration overridden

--- src/main/java/com/example/taskmanager/controller/LoggerInterceptor.java
+++ src/main/java/com/example/taskmanager/controller/LoggerInterceptor.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 class LoggerInterceptor implements HandlerInterceptor {
@@ -15,7 +16,8 @@ class LoggerInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
 	                         Object handler) throws Exception {
-		logger.info("[doFilter]" + request.getMethod() + " " + request.getRequestURI());
+		logger.info("[preHandler]" + request.getMethod() + " " + request.getRequestURI());
 		return true;
 	}
+
 }
--- src/main/java/com/example/taskmanager/controller/MvcConfiguration.java
+++ src/main/java/com/example/taskmanager/controller/MvcConfiguration.java
@@ -1,7 +1,14 @@
 package com.example.taskmanager.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class MvcConfiguration {
+class MvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor());
+	}
 }
