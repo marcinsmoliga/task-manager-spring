0a1a576ea0dbcc2fb09bb74b76d49d00bf3c3cbd
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-28 12:47:39 +0100

    dependency Actuator added and configured

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -5,10 +5,10 @@ import javax.validation.Validator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+@EnableAsync
 @SpringBootApplication
 public class TaskManagerApplication {
 
--- src/main/java/com/example/taskmanager/controller/TaskController.java
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -2,13 +2,13 @@ package com.example.taskmanager.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
