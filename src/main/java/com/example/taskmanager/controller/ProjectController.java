6dc7afccb3a0f4587d25d07dd2ef052c57d203cb
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-12-30 23:11:27 +0100

    addProject(+)/controller created

--- src/main/java/com/example/taskmanager/controller/ProjectController.java
+++ src/main/java/com/example/taskmanager/controller/ProjectController.java
@@ -7,17 +7,33 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.taskmanager.logic.ProjectService;
 import com.example.taskmanager.model.ProjectStep;
 import com.example.taskmanager.model.projection.ProjectWriteModel;
 
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
+
+	private final ProjectService service;
+
+	ProjectController(ProjectService service) {
+		this.service = service;
+	}
+
 	@GetMapping
 	String showProjects(Model model) {
 		var projectToEdit = new ProjectWriteModel();
 		projectToEdit.setDescription("project to edit");
 		model.addAttribute("project", projectToEdit);
+		return "projects";
+	}
+
+	@PostMapping
+	String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+		service.save(current);
+		model.addAttribute("project", new ProjectWriteModel());
+		model.addAttribute("message", "New Project has been Created and Saved");
 		return "projects";
 	}
 
