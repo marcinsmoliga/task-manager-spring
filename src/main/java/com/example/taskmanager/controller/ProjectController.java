08fd9cdce7bda83c21c83505f817cf5046935e2a
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-01 18:59:04 +0100

    createGroup(...)/ProjectController implemented

--- src/main/java/com/example/taskmanager/controller/ProjectController.java
+++ src/main/java/com/example/taskmanager/controller/ProjectController.java
@@ -1,14 +1,17 @@
 package com.example.taskmanager.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,6 +45,7 @@ class ProjectController {
 		}
 		service.save(current);
 		model.addAttribute("project", new ProjectWriteModel());
+		model.addAttribute("projects", getProjects());
 		model.addAttribute("message", "New Project has been Created and Saved");
 		return "projects";
 	}
@@ -49,6 +53,20 @@ class ProjectController {
 	@PostMapping(params ="addStep")
 	String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
 		current.getSteps().add(new ProjectStep());
+		return "projects";
+	}
+
+	@PostMapping("/{id}")
+	String createGroup(@ModelAttribute("project") ProjectWriteModel current,
+	                   Model model,
+	                   @PathVariable int id,
+	                   @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")LocalDateTime deadline) {
+		try {
+			service.createGroup(deadline, id);
+			model.addAttribute("message", "The Group has been added!");
+		} catch (IllegalStateException | IllegalArgumentException e) {
+			model.addAttribute("message", "An error occurred while creating the group!");
+		}
 		return "projects";
 	}
 
