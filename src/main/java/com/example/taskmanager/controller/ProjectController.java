f43e2dfd3175c2bc3493c7ef91f8746fd9b2e75b
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-01 15:35:31 +0100

    getProjects()/ProjectController created | code to display projects list added/projects.html

--- src/main/java/com/example/taskmanager/controller/ProjectController.java
+++ src/main/java/com/example/taskmanager/controller/ProjectController.java
@@ -1,5 +1,7 @@
 package com.example.taskmanager.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.taskmanager.logic.ProjectService;
+import com.example.taskmanager.model.Project;
 import com.example.taskmanager.model.ProjectStep;
 import com.example.taskmanager.model.projection.ProjectWriteModel;
 
@@ -41,6 +44,11 @@ class ProjectController {
 	String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
 		current.getSteps().add(new ProjectStep());
 		return "projects";
+	}
+
+	@ModelAttribute("projects")
+	List<Project> getProjects() {
+		return service.readAll();
 	}
 
 }
