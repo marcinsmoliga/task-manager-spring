bd1e83ea2214e7328efe865d8f64ec2b4257f8a1
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-07 20:31:41 +0100

    method configure(...)/SecurityConfiguration overridden

--- src/main/java/com/example/taskmanager/security/SecurityConfiguration.java
+++ src/main/java/com/example/taskmanager/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -33,5 +34,15 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 	@Override
 	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
 		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.authorizeRequests()
+				.antMatchers("/info/*")
+				.hasRole("USER")
+				.anyRequest()
+				.permitAll();
 	}
 }
